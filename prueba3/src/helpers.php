9b4613a2ca2d9b76cfb39dec3daf357c19c8438f
Aland3v <aland3v@gmail.com>
2021-02-27 21:57:01 -0400

    pequeños cambios

--- prueba3/src/helpers.php
+++ prueba3/src/helpers.php
@@ -1,9 +1,13 @@
 <?php
 
-function get_attrs($car) {
-    return "Color: ".$car->get_color().", Model: ".$car->get_model().", Brand: ".$car->get_brand()."<br />";
+if(!function_exists('get_attrs')) {
+    function get_attrs($car) {
+        return "Color: ".$car->get_color().", Model: ".$car->get_model().", Brand: ".$car->get_brand()."<br />";
+    }
 }
 
-function upper($text) {
-    return strtoupper($text);
+if(!function_exists('upper')) {
+    function upper($text) {
+        return strtoupper($text);
+    }
 }
